b4d2464b66eeb2a5024a9dcd7b58a43098d701cb
Fernando Hernandez <fernandopulpyto@gmail.com>
2026-05-19 21:16:59 -0600

    Se mejoro el script que ejecuta la imagen en qemu y se agrego la opcion de debug en qemu

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-qemu-system-i386 -drive file=venus.img,format=raw,if=floppy -m 1M -vga std
+if [ "$1" == "debug" ]; then
+
+	qemu-system-i386 \
+	-drive file=venus.img,format=raw,if=floppy \
+	-m 1M \
+	-vga std \
+	-s -S
+
+else
+
+	qemu-system-i386 \
+	-drive file=venus.img,format=raw,if=floppy \
+	-m 1M -vga std
+
+fi
